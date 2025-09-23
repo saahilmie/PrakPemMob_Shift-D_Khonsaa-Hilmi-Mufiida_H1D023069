c31a9fd092660d365181770cbe30e5b4a1e59713
saahilmie <khonsaahilmiee@gmail.com>
2025-09-23 22:57:57 +0700

    Tugas 2 Pertemuan 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.unsoed.informatikamobile"
     compileSdk = 36
 
+    viewBinding {
+        enable = true
+    }
+
     defaultConfig {
         applicationId = "com.unsoed.informatikamobile"
         minSdk = 24
@@ -42,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
